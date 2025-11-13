292cbf90067192b032076a485efe105c148ee654
JoinNico <joenikon04@gmail.com>
2025-11-13 11:51:38 +0800

    Create a new project on Gamepad

---
+++ PS211/Core/Inc/main.h
@@ -0,0 +1,133 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define JOY_XR_Pin GPIO_PIN_0
+#define JOY_XR_GPIO_Port GPIOC
+#define JOY_YR_Pin GPIO_PIN_1
+#define JOY_YR_GPIO_Port GPIOC
+#define GL_R_Pin GPIO_PIN_2
+#define GL_R_GPIO_Port GPIOC
+#define GL_L_Pin GPIO_PIN_3
+#define GL_L_GPIO_Port GPIOC
+#define JOY_XL_Pin GPIO_PIN_0
+#define JOY_XL_GPIO_Port GPIOA
+#define JOY_YL_Pin GPIO_PIN_1
+#define JOY_YL_GPIO_Port GPIOA
+#define OLED_RST_Pin GPIO_PIN_2
+#define OLED_RST_GPIO_Port GPIOA
+#define OLED_DC_Pin GPIO_PIN_3
+#define OLED_DC_GPIO_Port GPIOA
+#define OLED_CS_Pin GPIO_PIN_4
+#define OLED_CS_GPIO_Port GPIOA
+#define OLED_SCK_Pin GPIO_PIN_5
+#define OLED_SCK_GPIO_Port GPIOA
+#define OLED_MISO_Pin GPIO_PIN_6
+#define OLED_MISO_GPIO_Port GPIOA
+#define OLED_MOSI_Pin GPIO_PIN_7
+#define OLED_MOSI_GPIO_Port GPIOA
+#define BL_L_Pin GPIO_PIN_4
+#define BL_L_GPIO_Port GPIOC
+#define BL_R_Pin GPIO_PIN_5
+#define BL_R_GPIO_Port GPIOC
+#define WX_L_Pin GPIO_PIN_0
+#define WX_L_GPIO_Port GPIOB
+#define WX_R_Pin GPIO_PIN_1
+#define WX_R_GPIO_Port GPIOB
+#define NRF_IRQ_Pin GPIO_PIN_10
+#define NRF_IRQ_GPIO_Port GPIOB
+#define NRF_CE_Pin GPIO_PIN_11
+#define NRF_CE_GPIO_Port GPIOB
+#define NRF_CS_Pin GPIO_PIN_12
+#define NRF_CS_GPIO_Port GPIOB
+#define NRF_SCK_Pin GPIO_PIN_13
+#define NRF_SCK_GPIO_Port GPIOB
+#define NRF_MISO_Pin GPIO_PIN_14
+#define NRF_MISO_GPIO_Port GPIOB
+#define NRF_MOSI_Pin GPIO_PIN_15
+#define NRF_MOSI_GPIO_Port GPIOB
+#define JOY_ZL_Pin GPIO_PIN_6
+#define JOY_ZL_GPIO_Port GPIOC
+#define JOY_ZR_Pin GPIO_PIN_7
+#define JOY_ZR_GPIO_Port GPIOC
+#define KEY1_Pin GPIO_PIN_8
+#define KEY1_GPIO_Port GPIOC
+#define KEY2_Pin GPIO_PIN_9
+#define KEY2_GPIO_Port GPIOC
+#define BEEP_Pin GPIO_PIN_8
+#define BEEP_GPIO_Port GPIOA
+#define LED_Pin GPIO_PIN_11
+#define LED_GPIO_Port GPIOA
+#define IMU_CS_Pin GPIO_PIN_15
+#define IMU_CS_GPIO_Port GPIOA
+#define IMU_SCK_Pin GPIO_PIN_3
+#define IMU_SCK_GPIO_Port GPIOB
+#define IMU_MISO_Pin GPIO_PIN_4
+#define IMU_MISO_GPIO_Port GPIOB
+#define IMU_MOSI_Pin GPIO_PIN_5
+#define IMU_MOSI_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ PS211/cmake-build-debug/CMakeFiles/PS211.elf.dir/Core/Src/sysmem.c.obj.d
@@ -0,0 +1,20 @@
+CMakeFiles/PS211.elf.dir/Core/Src/sysmem.c.obj: \
+ C:\Users\Administrator1\Desktop\Gamepad\PS211\Core\Src\sysmem.c \
+ d:\program\ files\stmicroelectronics\stm32cube\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\errno.h \
+ d:\program\ files\stmicroelectronics\stm32cube\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\errno.h \
+ d:\program\ files\stmicroelectronics\stm32cube\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\reent.h \
+ d:\program\ files\stmicroelectronics\stm32cube\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\_ansi.h \
+ d:\program\ files\stmicroelectronics\stm32cube\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\newlib.h \
+ d:\program\ files\stmicroelectronics\stm32cube\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\_newlib_version.h \
+ d:\program\ files\stmicroelectronics\stm32cube\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\config.h \
+ d:\program\ files\stmicroelectronics\stm32cube\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\machine\ieeefp.h \
+ d:\program\ files\stmicroelectronics\stm32cube\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\features.h \
+ d:\program\ files\stmicroelectronics\stm32cube\gcc-arm-none-eabi-10.3-2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ d:\program\ files\stmicroelectronics\stm32cube\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\_types.h \
+ d:\program\ files\stmicroelectronics\stm32cube\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\machine\_types.h \
+ d:\program\ files\stmicroelectronics\stm32cube\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\machine\_default_types.h \
+ d:\program\ files\stmicroelectronics\stm32cube\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\lock.h \
+ d:\program\ files\stmicroelectronics\stm32cube\gcc-arm-none-eabi-10.3-2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ d:\program\ files\stmicroelectronics\stm32cube\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\stdint.h \
+ d:\program\ files\stmicroelectronics\stm32cube\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\_intsup.h \
+ d:\program\ files\stmicroelectronics\stm32cube\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\_stdint.h
